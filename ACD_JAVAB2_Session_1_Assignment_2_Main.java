06acceb0efaf7945e462e920e6dee7e866c7fa12
pmallik <puja_mallik@yahoo.com>
2016-12-07 20:41:57 +0530

    Add files via upload

---
+++ ACD_JAVAB2_Session_1_Assignment_2_Main.java
@@ -0,0 +1,27 @@
+
+
+public class ACD_JAVAB2_Session_1_Assignment_2_Main {
+
+	public static void main(String[] args) {
+		// TODO Result of some expressions
+		int a=20,a1=10;
+		
+		int b= a-- - --a;
+		
+		System.out.println("Result a-- - --a  " +b);
+		
+		int c=a--; 
+		
+		System.out.println("Result a-- " +c);
+		
+		
+		int d=a>>2;
+		
+		System.out.println("Result a>>2 " +d);
+		
+		int e=a&a1;  
+		
+		System.out.println("Result a&a1 " +e);
+	}
+
+}
